5a655e9c7abf525268c31295a169d744ae34dd7f
Stef van Stipdonk <stefvanstipdonk@gmail.com>
2024-04-25 17:26:51 +0200

    Added mov mode fiels 00, 01 and 10 and immediate to register version of move

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char nodisp[8][10] = {{"bx + si"}, {"bx + di"}, {"bp + si"},
+                      {"bp + di"}, {"si"},      {"di"},
+                      {"bp"},      {"bx"}
+
+};
+
 static uint8_t *read_file(const char *path, size_t *out_size) {
   FILE *f = fopen(path, "rb");
   assert(f != NULL);
@@ -24,7 +30,7 @@ static inline const char *get_register_name(int index, int is_word) {
   if (is_word) {
     return reg_word[index];
   }
-  
+
   return reg_byte[index];
 }
 
@@ -34,39 +40,87 @@ static void disasm(uint8_t *data, size_t size) {
 
   while (p < p_end) {
     uint8_t op0 = *p;
-    if ((op0 >> 2) != 0b100010) {
-      exit(EXIT_FAILURE);
-    }
+    if ((op0 >> 2) == 0b100010) { // MOV
 
-    const int w = op0 & 1;
-    const int d = (op0 >> 1) & 1;
+      const int w = op0 & 1;
+      const int d = (op0 >> 1) & 1;
 
-    const int is_word = w;
-    const int is_source_in_reg = !d;
-    const int is_destination_in_reg = !is_source_in_reg;
+      const int is_word = w;
+      const int is_source_in_reg = !d;
 
-    uint8_t op1 = p[1];
-    int rm = (op1) & ((1 << 3) - 1);
-    int reg = (op1 >> 3) & ((1 << 3) - 1);
-    int mod = (op1 >> 6) & ((1 << 2) - 1);
+      uint8_t op1 = p[1];
+      int rm = (op1) & 0b111;
+      int reg = (op1 >> 3) & 0b111;
+      int mod = (op1 >> 6) & 0b11;
 
-    if (mod == 0b11) {
-      int src, dest;
+      if (mod == 0b11) { // Register mode (no displacement)
+        int src, dest;
 
-      if (is_source_in_reg) {
-        src = reg;
-        dest = rm;
+        if (is_source_in_reg) {
+          src = reg;
+          dest = rm;
+        } else {
+          src = rm;
+          dest = reg;
+        }
+
+        printf("mov %s, %s ; Register mode (no displacement)\n",
+               get_register_name(dest, is_word),
+               get_register_name(src, is_word));
+
+        p += 2;
+      } else if (mod == 0b01) { // Memory Mode, 8-bit displacement follows
+        if (*((uint8_t *)p + 2) == 0) {
+          if (is_source_in_reg) {
+            printf("mov [%s], %s ; Memory mode, no displacement follows\n",
+                   nodisp[rm], get_register_name(reg, w));
+          } else {
+            printf("mov %s, [%s] ; Memory mode, no displacement follows\n",
+                   get_register_name(reg, w), nodisp[rm]);
+          }
+        } else {
+          printf(
+              "mov %s, [%s + %i] ; Memory mode, 8 bit displacement follows\n",
+              get_register_name(reg, w), nodisp[rm], *((uint8_t *)p + 2));
+        }
+        p += 3;
+      } else if (mod == 0b10) { // Memory Mode, 16-bit displacement follows
+        printf("mov %s, [%s + %i] ; Memory mode, 16 bit displacement follows\n",
+               get_register_name(reg, w), nodisp[rm], *((uint16_t *)(p + 2)));
+        p += 4;
+      } else if (mod == 0b00) { // Memory Mode, no displacement follows
+        if (rm == 0b110) {
+          p += 4;
+        } else {
+          if (is_source_in_reg) {
+            printf("mov [%s], %s ; Memory mode (no displacement)\n", nodisp[rm],
+                   get_register_name(reg, w));
+          } else {
+            printf("mov %s, [%s] ; Memory mode (no displacement)\n",
+                   get_register_name(reg, w), nodisp[rm]);
+          }
+
+          p += 2;
+        }
+      }
+    } else if ((op0 >> 4) == 0b1011) {
+      int reg = (op0) & 0b111;
+      int w = (op0 >> 3) & 1;
+
+      if (w == 1) {
+        printf("mov %s, %i ; Immediate to register 16 bit\n",
+               get_register_name(reg, w), *((uint16_t *)(p + 1)));
+        p += 3;
       } else {
-        src = rm;
-        dest = reg;
+        printf("mov %s, %i ; Immediate to register 8 bit\n",
+               get_register_name(reg, w), *((uint8_t *)(p + 1)));
+
+        p += 2;
       }
 
-      printf("mov %s, %s ; [%2x %2x] W=%d D=%d R/M=%d REG=%d MOD=%d\n",
-             get_register_name(dest, is_word), get_register_name(src, is_word),
-             op0, op1, w, d, rm, reg, mod);
+    } else {
+      p++;
     }
-
-    p += 2;
   }
 }
 int main(int argc, char **argv) {
